d3a752aabdbf9ad5e2f9ec7491b14506fc3389d9
Graham Goh <graham.goh@smartcontract.com>
2026-04-24 02:12:31 +1000

    [CLD-1916]: feat(solana): port MaybeLoadMCMSWithTimelockState (#12)
    * feat(solana): port MaybeLoadMCMSWithTimelockState
    
    Bring over [MaybeLoadMCMSWithTimelockState](https://github.com/smartcontractkit/chainlink/blob/7e663cb96847f78902ca9c61ae2b302931fd0434/deployment/common/changeset/state/solana.go#L170-L170) from chainlink repo
    
    JIRA:  https://smartcontract-it.atlassian.net/browse/CLD-1916
    
    * feat: more methods on MCMSWithTimelockPrograms

--- pkg/family/solana/state.go
+++ pkg/family/solana/state.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gagliardetto/solana-go"
+	timelockBindings "github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/v0_1_1/timelock"
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	mcmscontracts "github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/contracts/mcms"
@@ -43,7 +44,102 @@ type MCMSWithTimelockPrograms struct {
 	BypasserAccessControllerAccount  solana.PublicKey
 }
 
+func (s *MCMSWithTimelockPrograms) GetStateFromType(programType cldf.ContractType) (solana.PublicKey, PDASeed, error) {
+	switch programType {
+	case mcmscontracts.ManyChainMultisigProgram:
+		return s.McmProgram, PDASeed{}, nil
+	case mcmscontracts.ProposerManyChainMultisig:
+		return s.McmProgram, s.ProposerMcmSeed, nil
+	case mcmscontracts.BypasserManyChainMultisig:
+		return s.McmProgram, s.BypasserMcmSeed, nil
+	case mcmscontracts.CancellerManyChainMultisig:
+		return s.McmProgram, s.CancellerMcmSeed, nil
+	case mcmscontracts.RBACTimelockProgram:
+		return s.TimelockProgram, PDASeed{}, nil
+	case mcmscontracts.RBACTimelock:
+		return s.TimelockProgram, s.TimelockSeed, nil
+	case mcmscontracts.AccessControllerProgram:
+		return s.AccessControllerProgram, PDASeed{}, nil
+	case mcmscontracts.ProposerAccessControllerAccount:
+		return s.AccessControllerProgram, PDASeed(s.ProposerAccessControllerAccount), nil
+	case mcmscontracts.ExecutorAccessControllerAccount:
+		return s.AccessControllerProgram, PDASeed(s.ExecutorAccessControllerAccount), nil
+	case mcmscontracts.CancellerAccessControllerAccount:
+		return s.AccessControllerProgram, PDASeed(s.CancellerAccessControllerAccount), nil
+	case mcmscontracts.BypasserAccessControllerAccount:
+		return s.AccessControllerProgram, PDASeed(s.BypasserAccessControllerAccount), nil
+	default:
+		return solana.PublicKey{}, PDASeed{}, fmt.Errorf("unknown program type: %s", programType)
+	}
+}
+
+func (s *MCMSWithTimelockPrograms) SetState(contractType cldf.ContractType, program solana.PublicKey, seed PDASeed) error {
+	switch contractType {
+	case mcmscontracts.ManyChainMultisigProgram:
+		s.McmProgram = program
+	case mcmscontracts.ProposerManyChainMultisig:
+		s.McmProgram = program
+		s.ProposerMcmSeed = seed
+	case mcmscontracts.BypasserManyChainMultisig:
+		s.McmProgram = program
+		s.BypasserMcmSeed = seed
+	case mcmscontracts.CancellerManyChainMultisig:
+		s.McmProgram = program
+		s.CancellerMcmSeed = seed
+	case mcmscontracts.RBACTimelockProgram:
+		s.TimelockProgram = program
+	case mcmscontracts.RBACTimelock:
+		s.TimelockProgram = program
+		s.TimelockSeed = seed
+	case mcmscontracts.AccessControllerProgram:
+		s.AccessControllerProgram = program
+	case mcmscontracts.ProposerAccessControllerAccount:
+		s.ProposerAccessControllerAccount = program
+	case mcmscontracts.ExecutorAccessControllerAccount:
+		s.ExecutorAccessControllerAccount = program
+	case mcmscontracts.CancellerAccessControllerAccount:
+		s.CancellerAccessControllerAccount = program
+	case mcmscontracts.BypasserAccessControllerAccount:
+		s.BypasserAccessControllerAccount = program
+	default:
+		return fmt.Errorf("unknown contract type: %s", contractType)
+	}
+
+	return nil
+}
+
+func (s *MCMSWithTimelockPrograms) RoleAccount(role timelockBindings.Role) solana.PublicKey {
+	switch role {
+	case timelockBindings.Admin_Role:
+		return solana.PublicKey{}
+	case timelockBindings.Proposer_Role:
+		return s.ProposerAccessControllerAccount
+	case timelockBindings.Executor_Role:
+		return s.ExecutorAccessControllerAccount
+	case timelockBindings.Canceller_Role:
+		return s.CancellerAccessControllerAccount
+	case timelockBindings.Bypasser_Role:
+		return s.BypasserAccessControllerAccount
+	}
+
+	return solana.PublicKey{}
+}
+
 type PDASeed [32]byte
+
+// MaybeLoadMCMSWithTimelockState loads MCMSWithTimelockState for each provided chain selector from env.DataStore address refs.
+func MaybeLoadMCMSWithTimelockState(env cldf.Environment, chainSelectors []uint64) (map[uint64]*MCMSWithTimelockState, error) {
+	result := map[uint64]*MCMSWithTimelockState{}
+	for _, chainSelector := range chainSelectors {
+		state, err := MaybeLoadMCMSWithTimelockChainState(env.DataStore.Addresses().Filter(datastore.AddressRefByChainSelector(chainSelector)))
+		if err != nil {
+			return nil, fmt.Errorf("unable to load mcms and timelock solana chain state for chain selector %d: %w", chainSelector, err)
+		}
+		result[chainSelector] = state
+	}
+
+	return result, nil
+}
 
 // MaybeLoadMCMSWithTimelockChainState loads MCMSWithTimelockState from Datastore address refs.
 func MaybeLoadMCMSWithTimelockChainState(refs []datastore.AddressRef) (*MCMSWithTimelockState, error) {
--- pkg/family/solana/state_test.go
+++ pkg/family/solana/state_test.go
@@ -1,10 +1,18 @@
 package solana
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/gagliardetto/solana-go"
+	timelockBindings "github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/v0_1_1/timelock"
+	"github.com/smartcontractkit/chainlink-deployments-framework/chain"
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	mcmscontracts "github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/contracts/mcms"
+	"github.com/smartcontractkit/chainlink-deployments-framework/offchain/ocr"
+	"github.com/smartcontractkit/chainlink-deployments-framework/pkg/logger"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,4 +36,295 @@ func TestMaybeLoadMCMSWithTimelockChainState_NoMatchingRefs(t *testing.T) {
 		require.NotNil(t, got.MCMSWithTimelockPrograms)
 		require.Equal(t, solana.PublicKey{}, got.McmProgram)
 	})
+}
+
+func TestMaybeLoadMCMSWithTimelockState(t *testing.T) {
+	t.Parallel()
+
+	const chain1 uint64 = 100_001
+	const chain2 uint64 = 100_002
+	const mcmProgramAddr = "11111111111111111111111111111111"
+	const otherProgramAddr = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+
+	t.Run("nil and empty chain selector list", func(t *testing.T) {
+		t.Parallel()
+		env := testSolanaEnv(t, datastore.NewMemoryDataStore().Seal())
+
+		got, err := MaybeLoadMCMSWithTimelockState(env, nil)
+		require.NoError(t, err)
+		require.Empty(t, got)
+
+		got, err = MaybeLoadMCMSWithTimelockState(env, []uint64{})
+		require.NoError(t, err)
+		require.Empty(t, got)
+	})
+
+	t.Run("single chain no refs", func(t *testing.T) {
+		t.Parallel()
+		ds := datastore.NewMemoryDataStore()
+		env := testSolanaEnv(t, ds.Seal())
+		got, err := MaybeLoadMCMSWithTimelockState(env, []uint64{chain1})
+		require.NoError(t, err)
+		require.Len(t, got, 1)
+		st := got[chain1]
+		require.NotNil(t, st)
+		require.NotNil(t, st.MCMSWithTimelockPrograms)
+		require.Equal(t, solana.PublicKey{}, st.McmProgram)
+	})
+
+	t.Run("two chains isolated by selector", func(t *testing.T) {
+		t.Parallel()
+		ds := datastore.NewMemoryDataStore()
+		require.NoError(t, ds.Addresses().Add(datastore.AddressRef{
+			Address:       mcmProgramAddr,
+			ChainSelector: chain1,
+			Type:          datastore.ContractType(mcmscontracts.ManyChainMultisigProgram),
+		}))
+		require.NoError(t, ds.Addresses().Add(datastore.AddressRef{
+			Address:       otherProgramAddr,
+			ChainSelector: chain2,
+			Type:          datastore.ContractType(mcmscontracts.ManyChainMultisigProgram),
+		}))
+		require.NoError(t, ds.Addresses().Add(datastore.AddressRef{
+			Address:       otherProgramAddr,
+			ChainSelector: chain2,
+			Type:          datastore.ContractType(mcmscontracts.RBACTimelockProgram),
+		}))
+		env := testSolanaEnv(t, ds.Seal())
+		got, err := MaybeLoadMCMSWithTimelockState(env, []uint64{chain1, chain2})
+		require.NoError(t, err)
+		require.Len(t, got, 2)
+
+		wantMcm1 := solana.MustPublicKeyFromBase58(mcmProgramAddr)
+		wantMcm2 := solana.MustPublicKeyFromBase58(otherProgramAddr)
+		require.Equal(t, wantMcm1, got[chain1].McmProgram)
+		require.Equal(t, solana.PublicKey{}, got[chain1].TimelockProgram)
+
+		require.Equal(t, wantMcm2, got[chain2].McmProgram)
+		require.Equal(t, wantMcm2, got[chain2].TimelockProgram)
+	})
+
+	t.Run("invalid address returns wrapped error", func(t *testing.T) {
+		t.Parallel()
+		ds := datastore.NewMemoryDataStore()
+		require.NoError(t, ds.Addresses().Add(datastore.AddressRef{
+			Address:       "not-valid-base58",
+			ChainSelector: chain1,
+			Type:          datastore.ContractType(mcmscontracts.ManyChainMultisigProgram),
+		}))
+		env := testSolanaEnv(t, ds.Seal())
+		_, err := MaybeLoadMCMSWithTimelockState(env, []uint64{chain1})
+		require.ErrorContains(t, err, fmt.Sprintf(
+			"unable to load mcms and timelock solana chain state for chain selector %d: unable to parse public key from mcm address",
+			chain1))
+	})
+}
+
+func TestMCMSWithTimelockPrograms_GetStateFromType(t *testing.T) {
+	t.Parallel()
+
+	mcm := solana.MustPublicKeyFromBase58("11111111111111111111111111111111")
+	tlProg := solana.MustPublicKeyFromBase58("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+	acProg := solana.MustPublicKeyFromBase58("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL")
+
+	var seedProposer, seedBypass, seedCanc, seedTL PDASeed
+	seedProposer[0] = 1
+	seedBypass[1] = 2
+	seedCanc[2] = 3
+	for i := range seedTL {
+		seedTL[i] = byte(i + 1)
+	}
+
+	propAcc := solana.MustPublicKeyFromBase58("ComputeBudget111111111111111111111111111111")
+	execAcc := solana.MustPublicKeyFromBase58("SysvarRent111111111111111111111111111111111")
+	canAcc := solana.MustPublicKeyFromBase58("SysvarC1ock11111111111111111111111111111111")
+	bypassAcc := solana.MustPublicKeyFromBase58("Stake11111111111111111111111111111111111111")
+
+	s := &MCMSWithTimelockPrograms{
+		McmProgram:                       mcm,
+		ProposerMcmSeed:                  seedProposer,
+		BypasserMcmSeed:                  seedBypass,
+		CancellerMcmSeed:                 seedCanc,
+		TimelockProgram:                  tlProg,
+		TimelockSeed:                     seedTL,
+		AccessControllerProgram:          acProg,
+		ProposerAccessControllerAccount:  propAcc,
+		ExecutorAccessControllerAccount:  execAcc,
+		CancellerAccessControllerAccount: canAcc,
+		BypasserAccessControllerAccount:  bypassAcc,
+	}
+
+	tests := []struct {
+		name        string
+		programType cldf.ContractType
+		wantProgram solana.PublicKey
+		wantSeed    PDASeed
+	}{
+		{name: "ManyChainMultisigProgram", programType: mcmscontracts.ManyChainMultisigProgram, wantProgram: mcm, wantSeed: PDASeed{}},
+		{name: "ProposerManyChainMultisig", programType: mcmscontracts.ProposerManyChainMultisig, wantProgram: mcm, wantSeed: seedProposer},
+		{name: "BypasserManyChainMultisig", programType: mcmscontracts.BypasserManyChainMultisig, wantProgram: mcm, wantSeed: seedBypass},
+		{name: "CancellerManyChainMultisig", programType: mcmscontracts.CancellerManyChainMultisig, wantProgram: mcm, wantSeed: seedCanc},
+		{name: "RBACTimelockProgram", programType: mcmscontracts.RBACTimelockProgram, wantProgram: tlProg, wantSeed: PDASeed{}},
+		{name: "RBACTimelock", programType: mcmscontracts.RBACTimelock, wantProgram: tlProg, wantSeed: seedTL},
+		{name: "AccessControllerProgram", programType: mcmscontracts.AccessControllerProgram, wantProgram: acProg, wantSeed: PDASeed{}},
+		{name: "ProposerAccessControllerAccount", programType: mcmscontracts.ProposerAccessControllerAccount, wantProgram: acProg, wantSeed: PDASeed(propAcc)},
+		{name: "ExecutorAccessControllerAccount", programType: mcmscontracts.ExecutorAccessControllerAccount, wantProgram: acProg, wantSeed: PDASeed(execAcc)},
+		{name: "CancellerAccessControllerAccount", programType: mcmscontracts.CancellerAccessControllerAccount, wantProgram: acProg, wantSeed: PDASeed(canAcc)},
+		{name: "BypasserAccessControllerAccount", programType: mcmscontracts.BypasserAccessControllerAccount, wantProgram: acProg, wantSeed: PDASeed(bypassAcc)},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			gotProgram, gotSeed, err := s.GetStateFromType(tt.programType)
+			require.NoError(t, err)
+			require.Equal(t, tt.wantProgram, gotProgram)
+			require.Equal(t, tt.wantSeed, gotSeed)
+		})
+	}
+
+	t.Run("unknown program type", func(t *testing.T) {
+		t.Parallel()
+		_, _, err := s.GetStateFromType(cldf.ContractType("not-a-real-solana-program"))
+		require.ErrorContains(t, err, "unknown program type")
+	})
+}
+
+func TestMCMSWithTimelockPrograms_SetState(t *testing.T) {
+	t.Parallel()
+
+	mcm := solana.MustPublicKeyFromBase58("11111111111111111111111111111111")
+	tlProg := solana.MustPublicKeyFromBase58("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+	acProg := solana.MustPublicKeyFromBase58("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL")
+	roleAcc := solana.MustPublicKeyFromBase58("ComputeBudget111111111111111111111111111111")
+
+	var seed PDASeed
+	seed[7] = 42
+
+	t.Run("unknown contract type", func(t *testing.T) {
+		t.Parallel()
+		s := &MCMSWithTimelockPrograms{}
+		err := s.SetState(cldf.ContractType("unknown-contract"), mcm, seed)
+		require.ErrorContains(t, err, "unknown contract type")
+	})
+
+	t.Run("SetState then GetStateFromType", func(t *testing.T) {
+		t.Parallel()
+
+		roundTrips := []struct {
+			name         string
+			pre          func(*MCMSWithTimelockPrograms)
+			contractType cldf.ContractType
+			setProgram   solana.PublicKey
+			setSeed      PDASeed
+			wantProgram  solana.PublicKey
+			wantSeed     PDASeed
+			post         func(t *testing.T, s *MCMSWithTimelockPrograms)
+		}{
+			{
+				name:         "ManyChainMultisigProgram",
+				contractType: mcmscontracts.ManyChainMultisigProgram,
+				setProgram:   mcm,
+				wantProgram:  mcm,
+			},
+			{
+				name:         "ProposerManyChainMultisig",
+				contractType: mcmscontracts.ProposerManyChainMultisig,
+				setProgram:   mcm,
+				setSeed:      seed,
+				wantProgram:  mcm,
+				wantSeed:     seed,
+			},
+			{
+				name:         "RBACTimelockProgram",
+				contractType: mcmscontracts.RBACTimelockProgram,
+				setProgram:   tlProg,
+				wantProgram:  tlProg,
+			},
+			{
+				name:         "RBACTimelock",
+				contractType: mcmscontracts.RBACTimelock,
+				setProgram:   tlProg,
+				setSeed:      seed,
+				wantProgram:  tlProg,
+				wantSeed:     seed,
+			},
+			{
+				name:         "AccessControllerProgram",
+				contractType: mcmscontracts.AccessControllerProgram,
+				setProgram:   acProg,
+				wantProgram:  acProg,
+			},
+			{
+				name: "ProposerAccessControllerAccount",
+				pre: func(s *MCMSWithTimelockPrograms) {
+					s.AccessControllerProgram = acProg
+				},
+				contractType: mcmscontracts.ProposerAccessControllerAccount,
+				setProgram:   roleAcc,
+				wantProgram:  acProg,
+				wantSeed:     PDASeed(roleAcc),
+				post: func(t *testing.T, s *MCMSWithTimelockPrograms) {
+					t.Helper()
+					require.Equal(t, roleAcc, s.ProposerAccessControllerAccount)
+				},
+			},
+		}
+
+		for _, tt := range roundTrips {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				s := &MCMSWithTimelockPrograms{}
+				if tt.pre != nil {
+					tt.pre(s)
+				}
+				require.NoError(t, s.SetState(tt.contractType, tt.setProgram, tt.setSeed))
+				gotProgram, gotSeed, err := s.GetStateFromType(tt.contractType)
+				require.NoError(t, err)
+				require.Equal(t, tt.wantProgram, gotProgram)
+				require.Equal(t, tt.wantSeed, gotSeed)
+				if tt.post != nil {
+					tt.post(t, s)
+				}
+			})
+		}
+	})
+}
+
+func TestMCMSWithTimelockPrograms_RoleAccount(t *testing.T) {
+	t.Parallel()
+
+	propAcc := solana.MustPublicKeyFromBase58("ComputeBudget111111111111111111111111111111")
+	execAcc := solana.MustPublicKeyFromBase58("SysvarRent111111111111111111111111111111111")
+	canAcc := solana.MustPublicKeyFromBase58("SysvarC1ock11111111111111111111111111111111")
+	bypassAcc := solana.MustPublicKeyFromBase58("Stake11111111111111111111111111111111111111")
+
+	s := &MCMSWithTimelockPrograms{
+		ProposerAccessControllerAccount:  propAcc,
+		ExecutorAccessControllerAccount:  execAcc,
+		CancellerAccessControllerAccount: canAcc,
+		BypasserAccessControllerAccount:  bypassAcc,
+	}
+
+	require.Equal(t, propAcc, s.RoleAccount(timelockBindings.Proposer_Role))
+	require.Equal(t, execAcc, s.RoleAccount(timelockBindings.Executor_Role))
+	require.Equal(t, canAcc, s.RoleAccount(timelockBindings.Canceller_Role))
+	require.Equal(t, bypassAcc, s.RoleAccount(timelockBindings.Bypasser_Role))
+	require.Equal(t, solana.PublicKey{}, s.RoleAccount(timelockBindings.Admin_Role))
+	require.Equal(t, solana.PublicKey{}, s.RoleAccount(timelockBindings.Role(99)))
+}
+
+func testSolanaEnv(t *testing.T, ds datastore.DataStore) cldf.Environment {
+	t.Helper()
+	return *cldf.NewEnvironment(
+		"test",
+		logger.Nop(),
+		cldf.NewMemoryAddressBook(),
+		ds,
+		nil,
+		nil,
+		func() context.Context { return t.Context() },
+		ocr.OCRSecrets{},
+		chain.NewBlockChains(nil),
+	)
 }
