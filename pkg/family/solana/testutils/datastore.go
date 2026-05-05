d29a2771ed91d6a070157b7f7dba6e33e56b0cde
Pablo Estrada <139084212+ecPablo@users.noreply.github.com>
2026-05-05 13:08:29 -0600

    chore: port GetMCMSStateFromAddressBook (#35)

--- pkg/family/solana/testutils/datastore.go
+++ pkg/family/solana/testutils/datastore.go
@@ -3,12 +3,14 @@ package soltestutils
 import (
 	"testing"
 
+	cldfsolana "github.com/smartcontractkit/chainlink-deployments-framework/chain/solana"
 	mcmscontracts "github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/contracts/mcms"
 	"github.com/stretchr/testify/require"
 
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 
 	"github.com/smartcontractkit/cld-changesets/pkg/common"
+	solstate "github.com/smartcontractkit/cld-changesets/pkg/family/solana"
 	solutils "github.com/smartcontractkit/cld-changesets/pkg/family/solana/utils"
 )
 
@@ -32,4 +34,19 @@ func PreloadAddressBookWithMCMSPrograms(t *testing.T, selector uint64) *cldf.Add
 	require.NoError(t, err)
 
 	return ab
+}
+
+// GetMCMSStateFromAddressBook retrieves the state of the Solana MCMS contracts on the given chain.
+func GetMCMSStateFromAddressBook(
+	t *testing.T, ab cldf.AddressBook, chain cldfsolana.Chain,
+) *solstate.MCMSWithTimelockState {
+	t.Helper()
+
+	addresses, err := ab.AddressesForChain(chain.Selector)
+	require.NoError(t, err)
+
+	mcmState, err := solstate.MaybeLoadMCMSWithTimelockChainState(chain, addresses)
+	require.NoError(t, err)
+
+	return mcmState
 }
