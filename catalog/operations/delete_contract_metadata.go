4b75d9bfb2dec9b4f148161810d11adda43d74d3
Giorgio Gambino <151543+giogam@users.noreply.github.com>
2026-06-02 09:09:29 +0200

    feat: add datastore contract metadata changeset (#74)
    This pull request introduces a new changeset and supporting operation
    for deleting contract metadata entries in the Catalog service or from
    local datastore files
    
    CLD-2303

---
+++ catalog/operations/delete_contract_metadata.go
@@ -0,0 +1,49 @@
+package operations
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	cldfdatastore "github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	cldfops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
+)
+
+// DeleteContractMetadataDeps holds non-serializable dependencies for the DeleteContractMetadataOp operation.
+type DeleteContractMetadataDeps struct {
+	DataStore cldfdatastore.DataStore
+}
+
+// DeleteContractMetadataInput is the serializable input of a DeleteContractMetadataOp invocation.
+type DeleteContractMetadataInput struct {
+	ContractMetadataKeys []cldfdatastore.ContractMetadataKey
+}
+
+// DeleteContractMetadataOutput is the serializable output of a DeleteContractMetadataOp invocation.
+type DeleteContractMetadataOutput struct {
+	DataStore cldfdatastore.MutableDataStore
+}
+
+// DeleteContractMetadataOp deletes contract metadata entries from the Catalog service or local datastore files.
+var DeleteContractMetadataOp = cldfops.NewOperation(
+	"datastore-delete-contract-metadata",
+	semver.MustParse("1.0.0"),
+	"Delete contract metadata entries from the Catalog service or local datastore files",
+	func(b cldfops.Bundle, deps DeleteContractMetadataDeps, input DeleteContractMetadataInput) (DeleteContractMetadataOutput, error) {
+		dataStore := cldfdatastore.NewMemoryDataStore()
+		err := dataStore.Merge(deps.DataStore)
+		if err != nil {
+			return DeleteContractMetadataOutput{}, fmt.Errorf("failed to create memory data store: %w", err)
+		}
+
+		for i, key := range input.ContractMetadataKeys {
+			err = dataStore.ContractMetadata().RemoteDelete(key)
+			if err != nil {
+				return DeleteContractMetadataOutput{}, fmt.Errorf("failed to delete contract metadata entry %d in datastore: %w", i, err)
+			}
+		}
+
+		b.Logger.Infow("Catalog ContractMetadata successfully staged for deletion")
+
+		return DeleteContractMetadataOutput{DataStore: dataStore}, nil
+	},
+)
