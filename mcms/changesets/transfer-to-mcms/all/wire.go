dc658e03d0750b61a8c1d053fc24f131bc2fbc5c
Pablo <pablo.estrada@smartcontract.com>
2026-06-24 08:43:12 -0600

    fix: restore transfer to mcms changes

--- mcms/changesets/transfer-to-mcms/all/wire.go
+++
@@ -1,7 +0,0 @@
-// Package all blank-imports built-in MCMS transfer-to-mcms families and readers.
-package all
-
-import (
-	_ "github.com/smartcontractkit/cld-changesets/mcms/evm/readers"
-	_ "github.com/smartcontractkit/cld-changesets/mcms/evm/transfer-to-mcms"
-)
--- mcms/evm/transfer-to-mcms/generate.go
+++
@@ -1,5 +0,0 @@
-package evmtransfertomcms
-
-// Regenerate ownable contract operations from operations_gen_config.yaml.
-//
-//go:generate go run github.com/smartcontractkit/chainlink-deployments-framework/tools/operations-gen@v0.2.0 -config operations_gen_config.yaml
